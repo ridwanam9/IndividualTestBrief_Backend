b22647c83dc17dc0a600925c29d019c6cbb0170f
ridwanam9 <ridwanamirulmaulana8@gmail.com>
2025-04-18 16:31:18 +0700

    .

--- app/models.py
+++ app/models.py
@@ -8,7 +8,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     email = db.Column(db.String(120), unique=True)
-    password_hash = db.Column(db.String(128))  # tambahkan ini
+    password_hash = db.Column(db.String(128))
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
